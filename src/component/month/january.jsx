4647bd93a8efe20128d4c08a93f499e2388bc04e
ccconac <qwerpoiu91@naver.com>
2023-03-04 20:31:43 +0900

    ADD: january css 박스 배치

---
+++ src/component/month/january.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./january.css";
+
+const january = () => {
+    return (
+        <div className = "main">
+            <div className = "month"></div>
+            <div className = "week"></div>
+            <div className = "days1"></div>
+            <div className = "days2"></div>
+            <div className = "days3"></div>
+            <div className = "days4"></div>
+            <div className = "days5"></div>
+        </div>
+        
+    )
+}
+
+export default january;
