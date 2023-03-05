52dae3527e8d3b3f92e4ba7db54de3d64f9c2c78
ccconac <qwerpoiu91@naver.com>
2023-03-05 17:33:24 +0900

    ADD: 요일 및 날짜 추가, 폰트 css 추가

--- src/component/month/january.jsx
+++ src/component/month/january.jsx
@@ -4,15 +4,13 @@ import "./january.css";
 const january = () => {
     return (
         <div className = "main">
-            <div className = "month">1월</div>
-            <div className = "week"></div>
-            <div className = "days">
-                <div className = "days"></div>
-                <div className = "days"></div>
-                <div className = "days"></div>
-                <div className = "days"></div>
-                <div className = "days"></div>
-            </div>
+            <div className = "month">1월(여긴 이미지 삽입할거)</div>
+            <div className = "week">S M T W T F S</div>
+            <div className = "days1">01 02 03 04 05 06 07</div>
+            <div className = "days2">08 09 10 11 12 13 14</div>
+            <div className = "days3">15 16 17 18 19 20 21</div>
+            <div className = "days4">22 23 24 25 26 27 28</div>
+            <div className = "days5">29 30 31</div>
         </div>
         
     )
