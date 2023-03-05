94ce76dce69e8bce20ea45e5b5225922f508728f
ccconac <qwerpoiu91@naver.com>
2023-03-05 16:28:05 +0900

    FIX: css 중복, box margin, width 수정

--- src/component/month/january.jsx
+++ src/component/month/january.jsx
@@ -4,13 +4,15 @@ import "./january.css";
 const january = () => {
     return (
         <div className = "main">
-            <div className = "month"></div>
+            <div className = "month">1월</div>
             <div className = "week"></div>
-            <div className = "days1"></div>
-            <div className = "days2"></div>
-            <div className = "days3"></div>
-            <div className = "days4"></div>
-            <div className = "days5"></div>
+            <div className = "days">
+                <div className = "days"></div>
+                <div className = "days"></div>
+                <div className = "days"></div>
+                <div className = "days"></div>
+                <div className = "days"></div>
+            </div>
         </div>
         
     )
